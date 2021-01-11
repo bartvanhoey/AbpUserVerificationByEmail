b63d46b3294723d5bf9d7d65ef569162a4877d94
bartvanhoey <bartvanhoey@hotmail.com>
2021-01-11 14:20:20 +0100

    Update

--- src/AbpUserVerificationByEmail.Blazor/AbpUserVerificationByEmailBrandingProvider.cs
+++ src/AbpUserVerificationByEmail.Blazor/AbpUserVerificationByEmailBrandingProvider.cs
@@ -6,6 +6,6 @@ namespace AbpUserVerificationByEmail.Blazor
     [Dependency(ReplaceServices = true)]
     public class AbpUserVerificationByEmailBrandingProvider : DefaultBrandingProvider
     {
-        public override string AppName => "AbpUserVerificationByEmail";
+        public override string AppName => "User Email Verification";
     }
 }
